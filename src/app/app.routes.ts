b935265d98cdaf36c18187117018b4556eba7496
Govarthananofficial <dotcreatives6222@gmail.com>
2025-04-16 23:17:36 +0530

    ngfor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,7 @@ export const routes: Routes = [
 {
     path:"",loadComponent:()=>import("./core/home/home.component").then((t)=>t.HomeComponent)
 },
+{
+    path:"h",loadComponent:()=>import("./core/create/create.component").then((t)=>t.CreateComponent)
+}
 ];
--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
 declare function headerslider():any
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -11,5 +12,16 @@ export class HomeComponent implements AfterViewInit{
   ngAfterViewInit(): void {
     headerslider()
   }
+sliders:{img:string}[]=[
+  {
+img:'assets/image/img-1.jpg'
+  },
+  {
+    img:'assets/image/img-2.jpg'
+      },{
+        img:'assets/image/img-3.jpg'
+          }
 
+]
 }
+ 
